c9f5c378aa6cc7cdd83b25ca57b656f992c22f33
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-01-27 11:31:04 +0600

    Added => Numeric, string,  boolean data type practice

---
+++ first.js
@@ -0,0 +1,18 @@
+// Numeric data types
+console.log(50);
+
+var age = 40;
+console.log(age);
+
+var rollNumbers = (12, 54, 7, 122);
+console.log(rollNumbers);
+
+
+
+
+
+// string data types
+console.log("My first javascript");
+
+var myName = "Humaion Kobir";
+console.log(myName);
---
+++ math.js
@@ -0,0 +1,12 @@
+/** 1) Number ==> 55,56,87
+ * integer: 3, 5;
+ * float: decimal: 2, 5, 13, 67
+ * operation: addition, substraction, multipliceation, division, division, remainder
+ * percent,
+ * average
+ * comparison
+ * series;
+ * EXTRA
+ * Prime
+ * Premutation
+  */
